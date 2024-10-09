4289808e3f448b809f56dc232b13cd2fc0190985
oladapo <oladapoelijah49@gmail.com>
2024-10-09 09:31:21 +0100

    implemented file handling

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import fs from "fs";
+import { v2 as cloudinary } from "cloudinary";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    // Upload an image
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("File uploaded on cloudinary. FIle src: " + response.url);
+
+    //     once the file is uploaded, we would like to delete it from our server
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
